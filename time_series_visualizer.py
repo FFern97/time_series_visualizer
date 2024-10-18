be636f6ba5aaa4db749ce6910fcd226da7b3366a
Facundo Fernández <ffernanndez97@gmail.com>
2024-10-18 00:48:36 -0300

    Update time_series_visualizer.py

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,8 +5,8 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = pd.read_csv('fcc-forum-pageviews.csv')
-df.set_index('date')
+df = pd.read_csv('fcc-forum-pageviews.csv', parse_dates=['date'], index_col('date')
+
 
 # Clean data
 df_values = df[
